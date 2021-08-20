6f280956738b4bc6c8252334a2b9e47e090c4bfc
resyfer <resyfer.dev@gmail.com>
2021-08-20 15:11:02 +0530

    PhD done

--- src/pages/Phd.jsx
+++ src/pages/Phd.jsx
@@ -1,6 +1,16 @@
 //* React
 import { useEffect } from 'react';
 
+//* Dependencies
+import {
+	Timeline,
+	TimelineItem,
+	TimelineSeparator,
+	TimelineConnector,
+	TimelineContent,
+	TimelineDot,
+} from '@material-ui/lab';
+
 //* CSS
 import './css/Phd.scss';
 
@@ -19,104 +29,108 @@ const Phd = props => {
 				Ph.D./PG level course work is required.
 			</p>
 
-			{/* TODO: Add Styles */}
-			<div className='timeline'>
-				{/* Left Labels */}
-				<div className='tl'>
-					<div className='tl-left'>
-						<div className='tl-box-left'>
-							<div className='tl-time-left'>Journey Begins</div>
-							<div className='tl-txt-time'>Admission</div>
-						</div>
-
-						<div className='tl-mt-left'></div>
-
-						<div className='tl-box-left'>
-							<div className='tl-time-left'>3rd Sem</div>
-							<div className='tl-txt-time'>Comprehensive Examination</div>
-						</div>
-
-						<div className='tl-mt-left'></div>
-
-						<div className='tl-box-left'>
-							<div className='tl-time-left'>5th Sem onwards</div>
-							<div className='tl-txt-time'>Progress Seminar</div>
-						</div>
-
-						<div className='tl-mt-left gap'></div>
-
-						<div className='tl-box-left'>
-							<div className='tl-txt'>Thesis Submission</div>
-						</div>
-
-						<div className='tl-mt-left'></div>
-
-						<div className='tl-box-left'>
-							<div className='tl-time-left'>Journey Ends</div>
-							<div className='tl-txt-time'>Ph. D. Degree</div>
-						</div>
-					</div>
-
-					{/* Timeline Line */}
-					<div className='tl-middle'>
-						<div className='tl-line'>
-							<div className='tl-circle'></div>
-							<div className='tl-circle'></div>
-							<div className='tl-circle'></div>
-							<div className='tl-circle'></div>
-							<div className='tl-circle'></div>
-							<div className='tl-circle gap'></div>
-							<div className='tl-circle'></div>
-							<div className='tl-circle'></div>
-							<div className='tl-circle'></div>
-						</div>
-					</div>
-
-					{/* Right Labels */}
-					<div className='tl-right'>
-						<div className='tl-mt-right'></div>
-
-						<div className='tl-box-right'>
-							<div className='tl-time-right'>1st-2nd Sem</div>
-							<div className='tl-txt-time'>Course Work</div>
-						</div>
-
-						<div className='tl-mt-right'></div>
-
-						<div className='tl-box-right'>
-							<div className='tl-time-right'>4th Sem</div>
-							<div className='tl-txt-time'>Registration Seminar</div>
-						</div>
-
-						<div className='tl-mt-right'></div>
-
-						<div className='tl-box-right gap'>
-							<div className='tl-txt'>Synopsis Seminar</div>
-						</div>
-
-						<div className='tl-mt-right'></div>
-
-						<div className='tl-box-right'>
-							<div className='tl-txt'>Defense Seminar</div>
-						</div>
-
-						<div className='tl-mt-right'></div>
-					</div>
-				</div>
-			</div>
+			<Timeline align='alternate'>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot variant='outlined' />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent>
+						<span className='bold'>Journey Begins</span> | Admission
+					</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot variant='outlined' />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent>
+						<span className='bold'>1st-2nd Sem</span> | Course Work
+					</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot variant='outlined' />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent>
+						<span className='bold'>3rd Sem</span> | Comprehensive Examination
+					</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot variant='outlined' />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent>
+						<span className='bold'>4th Sem</span> | Registration Seminar
+					</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot variant='outlined' />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent>
+						<span className='bold'>5th Sem onwards</span> | Progress Seminar
+					</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot variant='outlined' />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent></TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot variant='outlined' />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent></TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot variant='outlined' />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent>Synopsis Seminar</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot variant='outlined' />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent>Thesis Submission</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot variant='outlined' />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent>Defense Seminar</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot variant='outlined' />
+					</TimelineSeparator>
+					<TimelineContent>
+						<span className='bold'>Journey Ends</span> | Ph. D. Degree
+					</TimelineContent>
+				</TimelineItem>
+			</Timeline>
 
 			<div className='download'>
-				<span>* New regulation w.e.f. 2014</span>
-				<span>
-					Click{' '}
-					<a
-						href='http://www.nits.ac.in/admissions/PG-Admission-2014/PhD_Regulation_July_2014.pdf'
-						target='_blank'
-						rel='noreferrer'>
-						here
-					</a>{' '}
-					to download the latest rules and regulations.
-				</span>
+				* New regulation w.e.f. 2014 <br />
+				Click{' '}
+				<a
+					href='http://www.nits.ac.in/admissions/PG-Admission-2014/PhD_Regulation_July_2014.pdf'
+					target='_blank'
+					rel='noreferrer'
+					className='bold'>
+					here
+				</a>{' '}
+				to download the latest rules and regulations.
 			</div>
 		</main>
 	);
