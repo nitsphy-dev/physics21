28c087a622ee9295cf065759b7bf2cc606b17eb2
Pratik Majumdar(codadept) <info.pratikmajumdar@gmail.com>
2021-08-20 12:36:44 +0530

    Added Faculty page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,141 +1,140 @@
 //* React Imports
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 //* CSS Imports
-import './css/Navbar.scss';
+import "./css/Navbar.scss";
 
 //* React Component
 const Navbar = () => {
-	const [mobNav, setMobNav] = useState(false);
+  const [mobNav, setMobNav] = useState(false);
 
-	useEffect(() => {
-		if (window.innerWidth >= 500) setMobNav(true);
-	}, [mobNav]);
+  useEffect(() => {
+    if (window.innerWidth >= 500) setMobNav(true);
+  }, [mobNav]);
 
-	return (
-		<header className='navbar'>
-			{/* Logo */}
-			<div className='navbar-logo'>Logo</div>
+  return (
+    <header className="navbar">
+      {/* Logo */}
+      <div className="navbar-logo">Logo</div>
 
-			{/* Mobile Menu */}
-			<div className='mob-menu'>
-				{!mobNav && (
-					<div
-						className='mob-menu-itm'
-						id='open'
-						onClick={() => setMobNav(true)}>
-						<i className='fas fa-bars'></i>
-					</div>
-				)}
-				{mobNav && (
-					<div
-						className='mob-menu-itm'
-						id='close'
-						onClick={() => setMobNav(false)}>
-						<i className='fas fa-times'></i>
-					</div>
-				)}
-			</div>
+      {/* Mobile Menu */}
+      <div className="mob-menu">
+        {!mobNav && (
+          <div
+            className="mob-menu-itm"
+            id="open"
+            onClick={() => setMobNav(true)}
+          >
+            <i className="fas fa-bars"></i>
+          </div>
+        )}
+        {mobNav && (
+          <div
+            className="mob-menu-itm"
+            id="close"
+            onClick={() => setMobNav(false)}
+          >
+            <i className="fas fa-times"></i>
+          </div>
+        )}
+      </div>
 
-			{/* Navbar Items */}
-			{mobNav && (
-				<div className='navbar-itm-ctnr'>
-					<Link to='/' className='navbar-itm scrl-top'>
-						Home
-					</Link>
-					<Link to='/about-us' className='navbar-itm scrl-top'>
-						About Us
-					</Link>
-					<Link to='/admissions' className='navbar-itm scrl-top'>
-						Admissions
-					</Link>
+      {/* Navbar Items */}
+      {mobNav && (
+        <div className="navbar-itm-ctnr">
+          <Link to="/" className="navbar-itm scrl-top">
+            Home
+          </Link>
+          <Link to="/about-us" className="navbar-itm scrl-top">
+            About Us
+          </Link>
+          <Link to="/admissions" className="navbar-itm scrl-top">
+            Admissions
+          </Link>
 
-					{/* Navbar Dropdown */}
-					<span className='navbar-itm scrl-top drpdwn'>
-						<span className='drpdwn-title'>
-							People&ensp;<i className='fas fa-chevron-down'></i>
-						</span>
-						<div className='drpdwn-menu'>
-							<Link to='/people/hod' className='drpdwn-itm'>
-								List of HODs
-							</Link>
-							<Link to='/people/faculty' className='drpdwn-itm'>
-								Faculty
-							</Link>
-							<Link to='/people/non-teaching-staff' className='drpdwn-itm'>
-								Non-Teaching Staff
-							</Link>
-							<Link to='/people/phd-scholars' className='drpdwn-itm'>
-								Ph.D. Scholars
-							</Link>
-							<Link to='/people/msc-scholars' className='drpdwn-itm'>
-								M.Sc. Students
-							</Link>
-							<Link to='/people/alumni' className='drpdwn-itm'>
-								Alumni
-							</Link>
-							<Link to='/people/dev-team' className='drpdwn-itm'>
-								Dev Team
-							</Link>
-						</div>
-					</span>
+          {/* Navbar Dropdown */}
+          <span className="navbar-itm scrl-top drpdwn">
+            <span className="drpdwn-title">
+              People&ensp;<i className="fas fa-chevron-down"></i>
+            </span>
+            <div className="drpdwn-menu">
+              <Link to="/people/faculty" className="drpdwn-itm">
+                Faculty
+              </Link>
+              <Link to="/people/non-teaching-staff" className="drpdwn-itm">
+                Non-Teaching Staff
+              </Link>
+              <Link to="/people/phd-scholars" className="drpdwn-itm">
+                Ph.D. Scholars
+              </Link>
+              <Link to="/people/msc-scholars" className="drpdwn-itm">
+                M.Sc. Students
+              </Link>
+              <Link to="/people/alumni" className="drpdwn-itm">
+                Alumni
+              </Link>
+              <Link to="/people/dev-team" className="drpdwn-itm">
+                Dev Team
+              </Link>
+            </div>
+          </span>
 
-					<span className='navbar-itm scrl-top drpdwn'>
-						<span className='drpdwn-title'>
-							Programs&ensp;<i className='fas fa-chevron-down'></i>
-						</span>
-						<div className='drpdwn-menu'>
-							<Link to='/programs/phd' className='drpdwn-itm'>
-								Doctoral Programs
-							</Link>
-							<Link to='/programs/msc' className='drpdwn-itm'>
-								Postgraduate Programs
-							</Link>
-							<Link to='/programs/btech' className='drpdwn-itm'>
-								Bachelor Programs
-							</Link>
-						</div>
-					</span>
+          <span className="navbar-itm scrl-top drpdwn">
+            <span className="drpdwn-title">
+              Programs&ensp;<i className="fas fa-chevron-down"></i>
+            </span>
+            <div className="drpdwn-menu">
+              <Link to="/programs/phd" className="drpdwn-itm">
+                Doctoral Programs
+              </Link>
+              <Link to="/programs/msc" className="drpdwn-itm">
+                Postgraduate Programs
+              </Link>
+              <Link to="/programs/btech" className="drpdwn-itm">
+                Bachelor Programs
+              </Link>
+            </div>
+          </span>
 
-					<span className='navbar-itm scrl-top drpdwn'>
-						<span className='drpdwn-title'>
-							Research&ensp;<i className='fas fa-chevron-down'></i>
-						</span>
-						<div className='drpdwn-menu'>
-							<Link to='/research/areas' className='drpdwn-itm'>
-								Research Areas
-							</Link>
-							<Link to='/research/facilities' className='drpdwn-itm'>
-								R&D Facilities
-							</Link>
-							<Link to='/research/projects' className='drpdwn-itm'>
-								Sponsored Projects
-							</Link>
-							<Link to='/research/publications' className='drpdwn-itm'>
-								Publications
-							</Link>
-							<Link to='/research/phd-thesis' className='drpdwn-itm'>
-								Ph.D. Thesis
-							</Link>
-						</div>
-					</span>
+          <span className="navbar-itm scrl-top drpdwn">
+            <span className="drpdwn-title">
+              Research&ensp;<i className="fas fa-chevron-down"></i>
+            </span>
+            <div className="drpdwn-menu">
+              <Link to="/research/areas" className="drpdwn-itm">
+                Research Areas
+              </Link>
+              <Link to="/research/facilities" className="drpdwn-itm">
+                R&D Facilities
+              </Link>
+              <Link to="/research/projects" className="drpdwn-itm">
+                Sponsored Projects
+              </Link>
+              <Link to="/research/publications" className="drpdwn-itm">
+                Publications
+              </Link>
+              <Link to="/research/phd-thesis" className="drpdwn-itm">
+                Ph.D. Thesis
+              </Link>
+            </div>
+          </span>
 
-					<Link to='/downloads' className='navbar-itm scrl-top'>
-						Downloads
-					</Link>
+          <Link to="/downloads" className="navbar-itm scrl-top">
+            Downloads
+          </Link>
 
-					<Link to='/gallery' className='navbar-itm scrl-top'>
-						Gallery
-					</Link>
+          <Link to="/gallery" className="navbar-itm scrl-top">
+            Gallery
+          </Link>
 
-					<Link to='/reach-us' className='navbar-itm scrl-top'>
-						Reach Us
-					</Link>
-				</div>
-			)}
-		</header>
-	);
+          <Link to="/reach-us" className="navbar-itm scrl-top">
+            Reach Us
+          </Link>
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Navbar;
