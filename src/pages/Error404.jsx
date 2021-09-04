73ebc62f5aafdd5c6558d18722aad42892857e02
Pratik Majumdar(codadept) <info.pratikmajumdar@gmail.com>
2021-09-05 02:53:24 +0530

    Error404 page design complete

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -6,17 +6,26 @@ import "./css/Error404.scss";
 
 //* React Component
 const Error404 = (props) => {
-
   //! Page Title
-  useEffect(()=> {
+  useEffect(() => {
     document.title = props.title;
-  }, [props.title])
+  }, [props.title]);
 
   return (
     <main className="Error404">
-      Error404...
+      <div className="error-main">
+        <div class="fof">
+          <h1>Error 404</h1>
+        </div>
+      </div>
+      <h4>Page not found</h4>
+      <p>
+        The page you are looking for doesn't exist or an other error occured.{" "}
+        <br />
+        <a href="/">Go back to homepage.</a>{" "}
+      </p>
     </main>
-  )
-}
+  );
+};
 
 export default Error404;
